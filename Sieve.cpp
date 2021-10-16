d6dc4039af1b4e79cfa80287bfb0cec6d618b8ee
puspitasamasi10 <92093017+puspitasamasi10@users.noreply.github.com>
2021-10-16 17:33:26 +0530

    Create Sieve.cpp

---
+++ Sieve.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+void simpleSieve(int lmt, vector<int>& prime) {
+   bool mark[lmt + 1];
+   memset(mark, false, sizeof(mark));
+   for (int i = 2; i <= lmt; ++i) {
+      if (mark[i] == false) {
+         prime.push_back(i);
+         for (int j = i; j <= lmt; j += i)
+            mark[j] = true;
+      }
+   }
+}
+void PrimeInRange(int low, int high) {
+   int lmt = floor(sqrt(high)) + 1;
+   vector<int> prime;
+   simpleSieve(lmt, prime);
+   int n = high - low + 1;
+   bool mark[n + 1];
+   memset(mark, false, sizeof(mark));
+   for (int i = 0; i < prime.size(); i++) {
+      int lowLim = floor(low / prime[i]) * prime[i];
+      if (lowLim < low)
+         lowLim += prime[i];
+      for (int j = lowLim; j <= high; j += prime[i])
+         mark[j - low] = true;
+   }
+   for (int i = low; i <= high; i++)
+      if (!mark[i - low])
+         cout << i << " ";
+}
+int main() {
+   int low = 10, high = 50;
+   PrimeInRange(low, high);
+   return 0;
+}
